2d6ecd042094904657bf4f9004056efa2e77e7ec
Brandon Smith <brandon.smith@yale.edu>
2014-10-26 15:13:31 -0500

    Update test to check for success message

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,7 +15,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation li', text: "Name can't be blank"
     assert_select 'div#error_explanation li', text: "Email is invalid"
     assert_select 'div#error_explanation li', text: "Password is too short (minimum is 6 characters)"
-    
+
     assert_select 'div.field_with_errors label[for=user_name]'
     assert_select 'div.field_with_errors label[for=user_email]'
     assert_select 'div.field_with_errors label[for=user_password]'
@@ -35,5 +35,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: password }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
+    assert_select 'div.alert.alert-success', text: "Welcome to the Sample App!"
   end
 end
